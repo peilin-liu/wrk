548ce6a4d3a92e802e0aa01a1bda2735c21de60f
liupeilin <2386985020@qq.com>
2016-08-11 19:09:27 +0800

    设置连接信息到lua

--- setup.lua
+++ setup.lua
@@ -8,7 +8,7 @@ local hot = 200 * 1000
 math.randomseed(os.time())
 
 wrk.scheme  = "http"
-wrk.host    = "192.168.3.20"
+wrk.host    = "192.168.3.137"
 wrk.port    = 8000
 wrk.method  = "GET"
 
@@ -23,10 +23,16 @@ num = 0
 local counter = 1
 local threads = {}
 
-function setup(thread)
+function setup(thread, cs)
+   print(string.format("setup cs=%d",cs))
    thread:set("id", counter)
+   thread:set("connections", tonumber(cs))
+   --print(string.format("setup connections=%d",connections))
+   thread:set("request_count", 0);
    table.insert(threads, thread)
    counter = counter + 1
+   local msg = "setup id=%d, sc=%d, rc=%d\n"
+   print(msg:format(thread:get("id"), thread:get("connections"), thread:get("request_count")))
 end
 
 
@@ -38,24 +44,46 @@ function init(args)
    print(msg:format(id, wrk.parallel_worker))
 end
 
-request = function()
-    local rand = math.random(hot, max)
-    if rand % 100 < 80 then
-        rand = rand % hot
-        headers["Oct-expires-default"] = "864000"
+function getlcm(m , n)
+	local a = m
+	local b = n
+	while a~=b do
+	   if a>b then  
+		 a=a-b      
+       else  
+		 b=b-a
+	   end
+	end 
+	return (m*n)/a
+end
 
+request = function(key)
+	local lcm = getlcm(wrk.parallel_worker, connections)
+    local localhot = hot - hot % lcm
+    local rand = math.random(localhot, max)
+    local single_hot = 0
+    if rand % 100 < 80 then
+        single_hot = math.floor(localhot/wrk.parallel_worker)
+		rand = single_hot*(id-1)
+		local single_pos = single_hot - single_hot%connections
+        rand = rand + single_pos + key
+
+        headers["Oct-expires-default"] = "864000"
     else
         headers["Oct-expires-default"] = "100"
+        rand = rand + hot
     end
-
+ 
     num = num + 1
     -- rand = num
 
     path = "/" .. rand
+	local msg = "request spec %d/%d %-6d %-8d/%4d %8d/%4d created"
+	print(msg:format(id, wrk.parallel_worker, single_hot, rand, key,localhot, lcm))
     return wrk.format("GET", path, headers)
 end
 
--- delay = function ()
---     return 20
---    --return math.random(1, 500)
--- end
+--delay = function ()
+--    return 20
+    --return math.random(1, 500)
+--end
